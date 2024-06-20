8d22720c2f084902d3079c76ccee0d4839b8e6e6
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2024-06-20 16:28:57 +0530

    Palindrome

--- DSA/Strings and StringBuilder/Palindrome.java
+++ DSA/Strings and StringBuilder/Palindrome.java
@@ -1,18 +1,19 @@
 public class Palindrome {
     public static void main(String[] args) {
-        String str="ababa";    
+        String str = "ababa";
         System.out.println(isPalindrome(str));
     }
-    static boolean isPalindrome(String str){
-        if (str.length()==0) {
+
+    static boolean isPalindrome(String str) {
+        if (str == null || str.length() == 0) {
             return true;
         }
-        str=str.toLowerCase();
-        for (int i = 0; i <= str.length()/2; i++) {
-            char start=str.charAt(i);
-            char end=str.charAt(str.length()-1-i);
-            if (start !=end) {
-                return false;     
+        str = str.toLowerCase();
+        for (int i = 0; i <= str.length() / 2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 - i);
+            if (start != end) {
+                return false;
             }
         }
         return true;
