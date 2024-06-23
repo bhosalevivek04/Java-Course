2e6946acc6de1385a063ebe7bf824d0b5b5303aa
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2024-06-23 18:27:16 +0530

    questions

---
+++ DSA/Recursion - Level 1 Questions/CountZeros.java
@@ -0,0 +1,19 @@
+public class CountZeros {
+    public static void main(String[] args) {
+        int n = 10001;
+        System.out.println(count(n));
+        }
+        static int count(int n){
+            return helper(n,0);
+        }
+        static int helper(int n, int c){
+            if(n==0){
+                return c;
+                }
+              int rem=n%10;
+               if (rem==0) {
+                    return helper(n/10, c+1);
+               } 
+               return helper(n/10, c);
+        }
+}
---
+++ DSA/Recursion - Level 1 Questions/Leetcode1342.java
@@ -0,0 +1,17 @@
+public class Leetcode1342 {
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(40));
+    }
+    public static int numberOfSteps(int num) {
+     return helper(num,0);   
+    }
+    static int helper(int num,int steps){
+        if (num==0) {
+            return steps;
+        }
+        if (num%2==0) {
+            return helper(num/2, steps+1);
+        }
+        return helper(num-1, steps+1);
+    }
+}
