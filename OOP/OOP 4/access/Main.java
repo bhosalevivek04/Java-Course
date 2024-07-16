9c96c7d4676b01637975bc14e70d46a79cba1d15
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2024-07-16 18:29:59 +0530

    Access Control, In-built Packages, Object Class

---
+++ OOP/OOP 4/access/Main.java
@@ -0,0 +1,9 @@
+package com.vivek.access;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj=new A(34,"Vivek");
+//        obj.setNum(25);
+        System.out.println(obj.getNum());
+    }
+}
