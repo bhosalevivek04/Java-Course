0a2e0345ebcdf9a4eec0d3c0d48b8b3a144c83d3
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2024-07-29 22:03:37 +0530

    Linked List using Recursion and some Leetcode problems

---
+++ DSA/Linked List/Leetcode83.java
@@ -0,0 +1,18 @@
+package com.vivek.linkedlist;
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/solutions/5553638/remove-duplicates-from-sorted-list/
+public class Leetcode83 {
+//    public ListNode deleteDuplicates(ListNode node) {
+//        if (node == null) {
+//            return node;
+//        }
+//        ListNode head = node;
+//        while (node.next != null) {
+//            if (node.val == node.next.val) {
+//                node.next = node.next.next;
+//            } else {
+//                node = node.next;
+//            }
+//        }
+//        return head;
+//    }
+}
--- DSA/Linked List/Main.java
+++ DSA/Linked List/Main.java
@@ -3,27 +3,30 @@ package com.vivek.linkedlist;
 
 public class Main {
     public static void main(String[] args) {
-    /*    LL list=new LL();
+        LL list=new LL();
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
         list.insertFirst(17);
 
-        list.insertLast(99);
-
-        list.insert(100,3);
-
+//        list.insertLast(99);
         list.display();
-        System.out.println(list.deteteFirst());
-
+        list.insertRec(88,2);
         list.display();
 
-        System.out.println(list.deleteLast());
-        list.display();
+//        list.insert(100,3);
+//
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//
+//        list.display();
+//
+//        System.out.println(list.deleteLast());
+//        list.display();
+//
+//        System.out.println(list.delete(2));
+//        list.display();
 
-        System.out.println(list.delete(2));
-        list.display();
-*/
 
 
 /*
@@ -57,7 +60,7 @@ public class Main {
         list.display();
 */
 
-        CLL list=new CLL();
+     /*   CLL list=new CLL();
         list.insert(23);
         list.insert(3);
         list.insert(19);
@@ -65,6 +68,6 @@ public class Main {
 
         list.display();
         list.delete(19);
-        list.display();
+        list.display(); */
     }
 }
