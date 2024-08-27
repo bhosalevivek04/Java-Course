38a91a17c8d278e0ea53ad43c1752b6ba54bcbbe
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2024-08-27 18:32:45 +0530

    Huffman Coding

---
+++ DSA/huffman/Main.java
@@ -0,0 +1,12 @@
+package com.vivek.huffman;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String str = "abbccda";
+        HuffmanCoder hf = new HuffmanCoder(str);
+        String cs = hf.encode(str);
+        System.out.println(cs);
+        String dc = hf.decode(cs);
+        System.out.println(dc);
+    }
+}
